5f242ef0890f741439b2ce004169a53aaa5bd94e
Konrad Fedorczyk <contact@realhe.ro>
2021-11-28 13:42:26 +0100

    Add local file content support

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,19 @@
+const path = require("path");
+
 module.exports = {
   siteMetadata: {
     siteUrl: "https:/fedek6.github.io",
     title: "Snippets",
   },
   pathPrefix: "/snippets",
-  plugins: ["gatsby-plugin-antd"],
+  plugins: [
+    "gatsby-plugin-antd",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "content",
+        path: path.join(__dirname, "content"),
+      },
+    },
+  ],
 };
