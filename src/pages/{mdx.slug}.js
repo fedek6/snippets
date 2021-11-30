d5809b141d8affe42bca8dc72dc10a4ef79bcff0
Konrad Fedorczyk <contact@realhe.ro>
2021-11-30 21:11:26 +0100

    Add category page

---
+++ src/pages/{mdx.slug}.js
@@ -0,0 +1,27 @@
+import * as React from "react";
+import { graphql } from "gatsby";
+import MdxBlock from "../components/mdx/MdxBlock";
+import Layout from "../components/layouts/HeaderSider";
+
+// markup
+const PlagroundPage = ({ data }) => (
+  <>
+    <Layout pageTitle={data.mdx.frontmatter.title}>
+      <MdxBlock>{data.mdx.body}</MdxBlock>
+    </Layout>
+  </>
+);
+
+export const query = graphql`
+  query ($id: String) {
+    mdx(id: { eq: $id }) {
+      frontmatter {
+        title
+        date(formatString: "MMMM D, YYYY")
+      }
+      body
+    }
+  }
+`;
+
+export default PlagroundPage;
