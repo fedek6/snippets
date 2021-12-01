f6b014bd87ed0b47d67da382c6526fac0f4ef523
Konrad Fedorczyk <contact@realhe.ro>
2021-12-01 16:20:58 +0100

    Clean up unused code

--- src/components/layouts/HeaderSider.js
+++ src/components/layouts/HeaderSider.js
@@ -33,16 +33,13 @@ const HeaderSider = ({ pageTitle, children }) => {
       <Layout>
         <Header className="header">
           <div className="logo" />
-          <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["2"]}>
+          <Menu theme="dark" mode="horizontal" defaultSelectedKeys={[data.allMdx.categories[0]]}>
             {
               data.allMdx.categories.map((category) => (
                   <Menu.Item key={category}>{ category }</Menu.Item>
                 )
               )
             }
-            <Menu.Item key="1">nav 1</Menu.Item>
-            <Menu.Item key="2">nav 2</Menu.Item>
-            <Menu.Item key="3">nav 3</Menu.Item>
           </Menu>
         </Header>
         <Layout>
