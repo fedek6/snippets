d9939a0f70fddb9ada446c37c7d06375003e68df
Konrad Fedorczyk <contact@realhe.ro>
2021-12-05 20:14:33 +0100

    Add index page for Gatsby

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,26 @@
 import * as React from "react";
+import { graphql } from "gatsby";
 import Layout from "../components/layouts/HeaderSider";
+import MdxBlock from "../components/mdx/MdxBlock";
 
 // markup
-const HomePage = ({ location }) => (
+const HomePage = ({ data, location }) => (
   <>
     <Layout pageTitle="home" location={location}>
-      Hello World!
+      <MdxBlock>{data.mdx.body}</MdxBlock>
     </Layout>
   </>
 );
+
+export const query = graphql`
+  query {
+    mdx(slug: { eq: "home" }) {
+      frontmatter {
+        title
+      }
+      body
+    }
+  }
+`;
 
 export default HomePage;
